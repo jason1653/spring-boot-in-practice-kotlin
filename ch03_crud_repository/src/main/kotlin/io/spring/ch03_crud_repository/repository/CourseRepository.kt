5e437aab629cb1f10ef70a817eb382368bde4137
jason <saga1654@kakao.com>
2023-12-04 10:41:51 +0900

    JPA Repository

--- ch03_crud_repository/src/main/kotlin/io/spring/ch03_crud_repository/repository/CourseRepository.kt
+++ ch03_crud_repository/src/main/kotlin/io/spring/ch03_crud_repository/repository/CourseRepository.kt
@@ -5,4 +5,13 @@ import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface CourseRepository : CrudRepository<Course, Long>
+interface CourseRepository : CrudRepository<Course, Long> {
+    fun findAllByCategory(category: String): List<Course>
+    fun findAllByCategoryOrderByName(category: String): List<Course>
+    fun existsByName(name: String): Boolean
+    fun countByCategory(category: String): Long
+    fun findByNameOrCategory(name: String, category: String): List<Course>
+    fun findByNameStartsWith(name: String): List<Course>
+    fun streamAllByCategory(category: String): List<Course>
+
+}
--- ch03_crud_repository/src/test/kotlin/io/spring/ch03_crud_repository/Ch03CrudRepositoryApplicationTests.kt
+++ ch03_crud_repository/src/test/kotlin/io/spring/ch03_crud_repository/Ch03CrudRepositoryApplicationTests.kt
@@ -13,6 +13,7 @@ class Ch03CrudRepositoryApplicationTests(
     private val courseRepository: CourseRepository,
 ) {
 
+    /*
     @Test
     fun givenCreateCourse() {
         val course = Course(
@@ -42,5 +43,70 @@ class Ch03CrudRepositoryApplicationTests(
 
         courseRepository.delete(course)
         assertThat(courseRepository.findById(savedCourse.id!!).isPresent).isFalse
+    }
+     */
+
+    @Test
+    fun givenCreateCourseWhenLoadTheCourceThenExpectSameCourse() {
+        courseRepository.saveAll(getCourse())
+
+        assertThat(courseRepository.findAllByCategory("Spring")).hasSize(3)
+        assertThat(courseRepository.existsByName("Javascript for All")).isTrue()
+        assertThat(courseRepository.existsByName("Mastering Javascript")).isFalse()
+        assertThat(courseRepository.countByCategory("Python")).isEqualTo(2)
+        assertThat(courseRepository.findByNameStartsWith("Getting Started")).hasSize(1)
+    }
+
+    private fun getCourse(): MutableIterable<Course> {
+        val data1 = Course(
+            name = "Rapid Spring Boot Application Development",
+            category = "Spring",
+            rating = 4,
+            description = "Kotlin is a modern programming language that makes developers happier.",
+        )
+
+        val data2 = Course(
+            name = "Getting Started with Spring Cloud Kubernetes",
+            category = "Spring",
+            rating = 5,
+            description = "Kotlin is a modern programming language that makes developers happier.",
+        )
+
+        val data3 = Course(
+            name = "Master Spring Boot application deployment with Kubernetes",
+            category = "Spring",
+            rating = 3,
+            description = "Kotlin is a modern programming language that makes developers happier.",
+        )
+
+        val data10 = Course(
+            name = "Master Python application deployment with Kubernetes",
+            category = "Python",
+            rating = 3,
+            description = "Kotlin is a modern programming language that makes developers happier.",
+        )
+
+        val data4 = Course(
+            name = "Game Development with Python",
+            category = "Python",
+            rating = 3,
+            description = "Kotlin is a modern programming language that makes developers happier.",
+        )
+
+        val data5 = Course(
+            name = "Javascript for All",
+            category = "Javascript",
+            rating = 4,
+            description = "Kotlin is a modern programming language that makes developers happier.",
+        )
+
+        val data6 = Course(
+            name = "Javascript Complate Guide",
+            category = "Javascript",
+            rating = 5,
+            description = "Kotlin is a modern programming language that makes developers happier.",
+        )
+
+        return mutableListOf(data1, data2, data3, data4, data5, data6, data10)
     }
 }
