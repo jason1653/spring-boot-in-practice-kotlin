6ac1b64f065f1a21bf4468454a3d3e65b932c721
jason <saga1654@kakao.com>
2023-12-08 18:15:19 +0900

    함수형 기반 route 설정

---
+++ ch08_webflux_functional/src/main/kotlin/io/spring/ch08_webflux_functional/configuration/AppContext.kt
@@ -0,0 +1,47 @@
+package io.spring.ch08_webflux_functional.configuration
+
+import io.spring.ch08_webflux_functional.model.Course
+import io.spring.ch08_webflux_functional.repository.CourseRepository
+import org.springframework.boot.CommandLineRunner
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import reactor.core.publisher.Flux
+
+@Configuration
+class AppContext {
+    @Bean
+    fun init(courseRepository: CourseRepository): CommandLineRunner {
+        return CommandLineRunner { args ->
+            val course1 = Course(
+                name = "Mastering Spring Boot",
+                category = "Spring",
+                rating = 5,
+                description = "Mastering Spring Boot 2.0",
+            )
+
+            val course2 = Course(
+                name = "Mastering Python",
+                category = "Python",
+                rating = 5,
+                description = "Mastering Python",
+            )
+
+            val course3 = Course(
+                name = "Mastering Go",
+                category = "Go",
+                rating = 3,
+                description = "Mastering Go",
+            )
+
+            Flux
+                .just(course1, course2, course3)
+                .flatMap { courseRepository.save(it) }
+                .thenMany(courseRepository.findAll())
+                .subscribe(
+                    { println(it) },
+                    { println("Error: $it.message") },
+                    { println("Completed") },
+                )
+        }
+    }
+}
--- ch08_webflux_functional/src/main/kotlin/io/spring/ch08_webflux_functional/configuration/RouterContext.kt
+++ ch08_webflux_functional/src/main/kotlin/io/spring/ch08_webflux_functional/configuration/RouterContext.kt
@@ -3,16 +3,22 @@ package io.spring.ch08_webflux_functional.configuration
 import io.spring.ch08_webflux_functional.component.CourseHandler
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.MediaType
 import org.springframework.web.reactive.function.server.RouterFunction
+import org.springframework.web.reactive.function.server.RouterFunctions.route
 import org.springframework.web.reactive.function.server.ServerResponse
-import org.springframework.web.reactive.function.server.coRouter
-import org.springframework.web.reactive.function.server.router
 
 @Configuration
 class RouterContext {
     @Bean
     fun routes(courseHandler: CourseHandler): RouterFunction<ServerResponse> {
+        return route()
+            .GET("/courses", courseHandler::findAllCourses)
+            .GET("/courses/{id}", courseHandler::findCourseById)
+            .POST("/courses", courseHandler::createCourse)
+            .PUT("/courses/{id}", courseHandler::updateCourse)
+            .DELETE("/courses/{id}", courseHandler::deleteCourse)
+            .DELETE("/courses", courseHandler::deleteAllCourses)
+            .build()
 
     }
 
